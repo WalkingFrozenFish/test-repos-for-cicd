2956f06de6ca51a7cbc5ce3bcb004ccc818a5218
Oleg Chachin <chachin.ol@yandex.kz>
2023-04-22 16:09:49 +0600

    Fix text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get("/", (req, res) => {
-    res.send("Some other response from server. Ebaniy ubuntu...")
+    res.send("Some other 1 response from server. Ebaniy ubuntu...")
 })
 
 app.listen(port, () => {
