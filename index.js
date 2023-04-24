35937162b8eeade41fd184e48f0eb24ce33ef649
Oleg Chachin <chachin.ol@yandex.kz>
2023-04-24 21:15:09 +0600

    Add endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("А мы офигеваем от сложности поставки")
 })
 
+app.get("/text/:sometext", (req, res) => {
+    res.send(req.params.sometext)
+})
+
 app.listen(port, () => {
     console.log(`Server work on ${port} port`)
 })
