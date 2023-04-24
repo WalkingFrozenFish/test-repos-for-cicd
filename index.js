f832a9b2f90a02ad0b0e1965158226f017a76e3b
Oleg Chachin <chachin.ol@yandex.kz>
2023-04-24 21:10:13 +0600

    Fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get("/", (req, res) => {
-    res.send("Тестовое сообщение")
+    res.send("А мы офигеваем от сложности поставки")
 })
 
 app.listen(port, () => {
